d0678d88596ecb91edf1adb5aec2e5b60da7a256
KuromiVoid1007 <coolvip1007@mail.ru>
2024-04-12 04:49:13 +0500

    Добавьте файлы проекта.

---
+++ GameWinform/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace GameWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.txtScore = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.txtPotion = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Location = new System.Drawing.Point(425, 630);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(37, 13);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Kills: 0";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(723, 612);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(100, 23);
+            this.healthBar.TabIndex = 2;
+            // 
+            // txtPotion
+            // 
+            this.txtPotion.AutoSize = true;
+            this.txtPotion.Location = new System.Drawing.Point(48, 612);
+            this.txtPotion.Name = "txtPotion";
+            this.txtPotion.Size = new System.Drawing.Size(35, 13);
+            this.txtPotion.TabIndex = 3;
+            this.txtPotion.Text = "label1";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = global::GameWinform.Properties.Resources.player;
+            this.player.Location = new System.Drawing.Point(477, 466);
+            this.player.Margin = new System.Windows.Forms.Padding(0);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(50, 50);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.txtPotion);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MaximumSize = new System.Drawing.Size(940, 700);
+            this.MinimumSize = new System.Drawing.Size(940, 700);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.Label txtPotion;
+    }
+}
+
---
+++ GameWinform/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameWinform
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, goUp, goDown, gameOver;
+        string facing = "Up";
+        int playerHealth = 100;
+        int speed = 10;
+        int ammo = 5;
+        int snakeSpeed = 3;
+
+        Random randNum = new Random();
+        int score;
+
+        List<PictureBox> snakesList = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (playerHealth > 1)
+            {
+                healthBar.Value = playerHealth;
+            }
+            else
+            {
+                gameOver = true;
+                player.Image = Properties.Resources.dead;
+                GameTimer.Stop();
+            }
+
+            txtPotion.Text = "Potion: " + ammo;
+            txtScore.Text = "kills: " + score;
+                
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+            if (goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+            if (goUp == true && player.Top > 45)
+            {
+                player.Top -= speed;
+            }
+            if (goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+
+
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "ammo")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+                        ammo += 5;
+
+                    }
+                }
+
+
+                if (x is PictureBox && (string)x.Tag == "snake")
+                {
+
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        playerHealth -= 1;
+                    }
+
+
+                    if (x.Left > player.Left)
+                    {
+                        x.Left -= snakeSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.sleft;
+                    }
+                    if (x.Left < player.Left)
+                    {
+                        x.Left += snakeSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.sright;
+                    }
+                    if (x.Top > player.Top)
+                    {
+                        x.Top -= snakeSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.sup;
+                    }
+                    if (x.Top < player.Top)
+                    {
+                        x.Top += snakeSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.sdown;
+                    }
+
+                }
+
+
+
+                foreach (Control j in this.Controls)
+                {
+                    if (j is PictureBox && (string)j.Tag == "potion" && x is PictureBox && (string)x.Tag == "snake")
+                    {
+                        if (x.Bounds.IntersectsWith(j.Bounds))
+                        {
+                            score++;
+
+                            this.Controls.Remove(j);
+                            ((PictureBox)j).Dispose();
+                            this.Controls.Remove(x);
+                            ((PictureBox)x).Dispose();
+                            snakesList.Remove(((PictureBox)x));
+                            MakeMonster();
+                        }
+                    }
+                }
+            }
+        }
+        
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+            if (gameOver == true)
+            {
+                return;
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+                facing = "A";
+                player.Image = Properties.Resources.left;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = true;
+                facing = "D";
+                player.Image = Properties.Resources.right;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = true;
+                facing = "W";
+                player.Image = Properties.Resources.up;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = true;
+                facing = "S";
+                player.Image = Properties.Resources.down;
+            }
+
+
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = false;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = false;
+            }
+
+            if (e.KeyCode == Keys.Space && ammo > 0 && gameOver == false)
+            {
+                ammo--;
+                ShootPotion(facing);
+
+
+                if (ammo < 1)
+                {
+                    DropAmmo();
+                }
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KeyPreview = true;
+        }
+
+        private void ShootPotion(string direction)
+        {
+            Potion ShootPotion = new Potion();
+            ShootPotion.direction = direction;
+            ShootPotion.potionLeft = player.Left + (player.Width / 2);
+            ShootPotion.potionTop = player.Top + (player.Height / 2);
+            ShootPotion.MakePotion(this);
+        }
+
+        private void MakeMonster()
+        {
+            PictureBox monster = new PictureBox();
+            monster.BackColor = Color.Transparent;
+            monster.Tag = "snake";
+            monster.Image = Properties.Resources.sdown;
+            monster.Left = randNum.Next(0, 900);
+            monster.Top = randNum.Next(0, 800);
+            monster.SizeMode = PictureBoxSizeMode.AutoSize;
+            snakesList.Add(monster);
+            this.Controls.Add(monster);
+            player.BringToFront();
+        }
+
+        private void DropAmmo()
+        {
+
+            PictureBox ammo = new PictureBox();
+            ammo.Image = Properties.Resources.potion_img;
+            ammo.SizeMode = PictureBoxSizeMode.AutoSize;
+            ammo.Left = randNum.Next(10, this.ClientSize.Width - ammo.Width);
+            ammo.Top = randNum.Next(60, this.ClientSize.Height - ammo.Height);
+            ammo.Tag = "ammo";
+            this.Controls.Add(ammo);
+
+            ammo.BringToFront();
+            player.BringToFront();
+        }
+
+        private void RestartGame()
+        {
+            player.Image = Properties.Resources.up;
+
+            foreach (PictureBox i in snakesList)
+            {
+                this.Controls.Remove(i);
+            }
+
+            snakesList.Clear();
+
+            for (int i = 0; i < 3; i++)
+            {
+                MakeMonster();
+            }
+
+            goUp = false;
+            goDown = false;
+            goLeft = false;
+            goRight = false;
+            gameOver = false;
+
+            playerHealth = 100;
+            score = 0;
+            ammo = 10;
+
+            GameTimer.Start();
+        }
+
+    }
+}
