1c757d6c3bad88f757d936ad0614d5edc400c4f8
KuromiVoid1007 <coolvip1007@mail.ru>
2024-04-17 00:34:43 +0500

    mvc

---
+++ GameWinform/Controler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameWinform
+{
+    internal class Controler
+    {
+
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+            if (gameOver == true)
+            {
+                return;
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+                facing = "A";
+                player.Image = Properties.Resources.left;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = true;
+                facing = "D";
+                player.Image = Properties.Resources.right;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = true;
+                facing = "W";
+                player.Image = Properties.Resources.up;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = true;
+                facing = "S";
+                player.Image = Properties.Resources.down;
+            }
+
+
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = false;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = false;
+            }
+
+            if (e.KeyCode == Keys.Space && ammo > 0 && gameOver == false)
+            {
+                ammo--;
+                ShootPotion(facing);
+
+
+                if (ammo < 1)
+                {
+                    DropAmmo();
+                }
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+    }
+}
